b2a5e6a7aa2464ea1e007f9e21b4468d403a6f79
hanbin8269 <hanbin8269@gmail.com>
2021-09-29 00:54:12 +0900

    feat: Add create fastapi app factory

---
+++ src/__init__.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+
+def init_middleware():
+    pass
+
+
+def init_orm():
+    pass
+
+
+def init_router():
+    pass
+
+
+def init_docs():
+    pass
+
+
+def create_app() -> FastAPI:
+    app = FastAPI()
+    return app
